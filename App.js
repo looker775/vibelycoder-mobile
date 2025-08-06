4c88cb4f22f0177577e28efabaf88706f2713455
looker775 <kaliwill3@gmail.com>
2025-08-06 20:28:27 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // VibelyCoderApp/App.js
 
 import React from 'react';
@@ -11,17 +10,3 @@ const App = () => (
 );
 
 export default App;
-=======
-// VibelyCoderApp/App.js
-
-import React from 'react';
-import { Text, View } from 'react-native';
-
-const App = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Hello from VibelyCoder 👋</Text>
-  </View>
-);
-
-export default App;
->>>>>>> d3789ba9d656bf8fbcaeb1db89e3ef8928847952
